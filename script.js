709dd7581c98a5d9481de579b2f97fbd6d00635a
Carolzita Dev <42356399+devcarolzita@users.noreply.github.com>
2024-11-06 16:09:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 import { images } from './dados.js';
 const galleryContainer = document.querySelector('#gallery-container');
 const loadMoreButton = document.querySelector('#load-more');
-console.log(loadMoreButton);
-
-console.log(galleryContainer, loadMoreButton);
 const imagesPerPage = 4;
 let currentImageIndex = 0;
 
 function loadImages () {
-  //Criar a div da imagem
-  //Criar a imagem
-  //E o source pegar do arquivos images.src
-  const pageImages = images.slice(currentImageIndex, currentImageIndex + imagesPerPage);
-  
-  console.log(pageImages);
-  
-  //For -> 
+  const pageImages = images.slice(currentImageIndex, currentImageIndex + imagesPerPage);  
   pageImages.forEach(images => {    
     const galleryItem = document.createElement('div');
     galleryItem.className = `gallery-item ${images.wide ? 'wide' : ''}`;
@@ -24,10 +14,8 @@ function loadImages () {
     imgItem.src = images.src;
     imgItem.alt = images.alt;
 
-
     galleryItem.appendChild(imgItem);
     galleryContainer.appendChild(galleryItem);
-
   });
 
   currentImageIndex += imagesPerPage;
@@ -40,18 +28,9 @@ function loadImages () {
 loadMoreButton.addEventListener('click', loadImages);
 
 window.addEventListener('scroll', () => {
-  console.log('teste');
-
-  //Altura da janela
   const viewportHeight = window.innerHeight;
-
-  //Posição atual do scroll
   const scrollPosition = window.scrollY;
-
-  //Altura total da página
   const pageHeight = document.body.offsetHeight;
-
-  //Antes de chegar até o final da página
   const offset = pageHeight - 100;
 
   if(viewportHeight + scrollPosition >= offset){
